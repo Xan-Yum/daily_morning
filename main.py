9d52d2125c5e4fa35bb13ff12fb69e1ad0de83a0
Xan-Yum <56751716+Xan-Yum@users.noreply.github.com>
2022-09-14 20:10:50 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import math
 from wechatpy import WeChatClient
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 from borax.calendars.lunardate import LunarDate
-import requests
 import os
+import requests
 import random
 
 today_solar = datetime.now()
@@ -28,56 +28,21 @@ app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
 user_ids = os.environ["USER_ID"].split(",")
 template_id = os.environ["TEMPLATE_ID"]
+url = os.environ["URL"]
 
-birthday = []
 data = {}
 
 
-# 获取生日列表
-def get_birth_lsit():
-    for index, i in enumerate(birthday_lunar):
-        birthday.append(i)
-    for index, j in enumerate(birthday_solar):
-        birthday.append(j)
-    return birthday
-
-
-# 生日农历改阳历
-def get_lunar_birth(birth):
-    # 为读取农历生日准备
-    lubaryear = today_lunar.year
-    if len(birth) == 10:
-        x = int(birth[0:4:1])  # 读取无用，为理解下面两行留着，可删去
-        y = int(birth[5:7])  # 切片
-        z = int(birth[8:10])
-    elif len(birth) == 5:
-        y = int(birth[0:2])  # 切片
-        z = int(birth[3:5])
-    birth1 = LunarDate(lubaryear, y, z)  # 构建今年农历生日日期
-    return birth1.to_solar_date().strftime('%m-%d')  # 转化成公历日期，输出为字符串
-
-
-# 批量生日农历改阳历
-def lunar_birth_list(birth):
-    for index, i in enumerate(birth):
-        birth[index] = get_lunar_birth(i)
-    return birth
-
-
 # 获取天气数据
-def get_weather():
+def get_weather(city):
     url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
-    url1 = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city1
     res = requests.get(url).json()
-    res1 = requests.get(url1).json()
     weather = res['data']['list'][0]
-    weather1 = res1['data']['list'][0]
-    return weather['city'], weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low']),\
-           weather1['city'], weather1['weather'], math.floor(weather1['temp']), math.floor(weather1['high']), math.floor(weather1['low'])
+    return weather['city'], weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
 
 
 # 获取疫情数据
-def get_virus():
+def get_virus(province, city):
     url = "https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=diseaseh5Shelf"
     req = requests.get(url)
     req_json = req.json()['data']['diseaseh5Shelf']['areaTree'][0]['children']
@@ -88,17 +53,9 @@ def get_virus():
             for j in range(1, len(req_json[i]['children'])):
                 if req_json[i]['children'][j]['name'] == city:
                     req_data = req_json[i]['children'][j]['today']
-                    # return req_data['local_confirm_add'], req_data['wzz_add']
+                    return req_data['local_confirm_add'], req_data['wzz_add']
                     # print(req_json[i]['children'][j]['name'])
         # print(req_json)
-    for i in range(1, len(req_json)):
-        if req_json[i]['name'] == province1:
-            # print(req_json[i])
-            for j in range(1, len(req_json[i]['children'])):
-                if req_json[i]['children'][j]['name'] == city1:
-                    req_data1 = req_json[i]['children'][j]['today']
-
-    return req_data['local_confirm_add'], req_data['wzz_add'], req_data1['local_confirm_add'], req_data1['wzz_add']
 
 
 # 获取纪念日天数
@@ -114,20 +71,27 @@ def get_exam_count():
 
 
 # 获取生日倒计时天数
-def get_birthday(birth):
-    if len(birth) == 10:
-        birth = birth[5:10]
-    next = datetime.strptime(str(date.today().year) + "-" + birth, "%Y-%m-%d")
-    if next < datetime.now():
-        next = next.replace(year=next.year + 1)
-    return (next - today_solar).days
-
-
-# 批量获取生日倒计时
-def birth_countdown_list(birth):
-    for index, i in enumerate(birth):
-        birth[index] = get_birthday(i)
-    return birth
+def get_birthday(index, birth):
+    if 'h' in index:
+        if len(birth) == 10:
+            birth = birth[5:10]
+        next = datetime.strptime(str(date.today().year) + "-" + birth, "%Y-%m-%d")
+        if next < today_solar:
+            next = next.replace(year=next.year + 1)
+        return (next - today_solar).days
+        # 为读取农历生日准备
+    elif 'g' in index:
+        if len(birth) == 10:
+            x = int(birth[0:4:1])  # 读取无用，为理解下面两行留着，可删去
+            y = int(birth[5:7])  # 切片
+            z = int(birth[8:10])
+        elif len(birth) == 5:
+            y = int(birth[0:2])  # 切片
+            z = int(birth[3:5])
+        next = LunarDate(today_lunar.year, y, z)
+        if next < today_lunar:
+            next = next.replace(year=next.year + 1)
+        return (next - today_lunar).days
 
 
 # 获取每日一言
@@ -187,23 +151,15 @@ client.menu.create({
 
 
 client = WeChatClient(app_id, app_secret)
-
 wm = WeChatMessage(client)
 
-# 批量生日农历改阳历
-birthday_lunar = lunar_birth_list(birthday_lunar)
-
-# 获取生日列表
-birthday = get_birth_lsit()
-
-# 获取所有生日倒计时
-birth_countdown_list = birth_countdown_list(birthday)
-
 # 获取天气数据
-ci, wea, temperature, highest, lowest, ci1, wea1, temperature1, highest1, lowest1 = get_weather()
+ci, wea, temperature, highest, lowest = get_weather(city)
+ci1, wea1, temperature1, highest1, lowest1 = get_weather(city1)
 
 # 获取疫情数据
-local_confirm_add, wzz_add, local_confirm_add1, wzz_add1 = get_virus()
+local_confirm_add, wzz_add = get_virus(province, city)
+local_confirm_add1, wzz_add1 = get_virus(province1, city1)
 
 # 所有模板数据
 data = {
@@ -213,22 +169,31 @@ data = {
     "4": {"value": wea, "color": get_random_color()},
     "5": {"value": str(temperature) + "℃", "color": get_random_color()},
     "6": {"value": str(lowest) + "℃ ~ " + str(highest) + "℃", "color": get_random_color()},
-    "7": {"value": get_count(), "color": get_random_color()},
-    "8": {"value": ci1, "color": get_random_color()},
-    "9": {"value": wea1, "color": get_random_color()},
-    "a": {"value": str(temperature1) + "℃", "color": get_random_color()},
-    "b": {"value": str(lowest1) + "℃ ~ " + str(highest1) + "℃", "color": get_random_color()},
-    "c": {"value": get_exam_count(), "color": get_random_color()},
-    "d": {"value": get_words(), "color": get_random_color()},
-    "f": {"value": local_confirm_add, "color": get_random_color()},
-    "g": {"value": wzz_add, "color": get_random_color()},
-    "h": {"value": local_confirm_add1, "color": get_random_color()},
-    "i": {"value": wzz_add1, "color": get_random_color()}}
+    "7": {"value": local_confirm_add, "color": get_random_color()},
+    "8": {"value": wzz_add, "color": get_random_color()},
+    "9": {"value": ci1, "color": get_random_color()},
+    "a": {"value": wea1, "color": get_random_color()},
+    "b": {"value": str(temperature1) + "℃", "color": get_random_color()},
+    "c": {"value": str(lowest1) + "℃ ~ " + str(highest1) + "℃", "color": get_random_color()},
+    "d": {"value": local_confirm_add1, "color": get_random_color()},
+    "e": {"value": wzz_add1, "color": get_random_color()},
+    "f": {"value": get_exam_count(), "color": get_random_color()},
+    "i": {"value": get_count(), "color": get_random_color()},
+    "j": {"value": get_words(), "color": get_random_color()}}
 
-for index, i in enumerate(birthday):
-    e = "e%d" % index
-    data[e] = {
-        "value": i,
+# 获取农历生日倒计时
+for index, i in enumerate(birthday_lunar):
+    g = "g%d" % index
+    data[g] = {
+        "value": get_birthday(g, i),
+        "color": get_random_color()
+    }
+
+# 获取阳历生日倒计时
+for index, i in enumerate(birthday_solar):
+    h = "h%d" % index
+    data[h] = {
+        "value": get_birthday(h, i),
         "color": get_random_color()
     }
 
